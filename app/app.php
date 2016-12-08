124ced7a63c3c055131e32a95f19c589ffb223b8
Dominik Zogg <dominik.zogg@gmail.com>
2016-12-08 20:48:18 +0100

    remove Prophiler

--- app/app.php
+++ app/app.php
@@ -6,10 +6,6 @@ $container = require_once __DIR__.'/bootstrap.php';
 
 $app = new App($container);
 
-if ($container['debug']) {
-    require_once __DIR__.'/profiler.php';
-}
-
 require_once __DIR__.'/middlewares.php';
 require_once __DIR__.'/controllers.php';
 
--- app/middlewares.php
+++ app/middlewares.php
@@ -1,6 +1,5 @@
 <?php
 
-use bitExpert\Http\Middleware\Psr7\Prophiler\ProphilerMiddleware;
 use Chubbyphp\Lazy\LazyMiddleware;
 use Negotiation\LanguageNegotiator;
 use Slim\App;
@@ -22,7 +21,3 @@ $app->add(new LazyMiddleware($container, 'csrf.middleware'));
 $app->add(new LazyMiddleware($container, 'session.middleware'));
 $app->add(new LazyMiddleware($container, LocaleMiddleware::class));
 $app->add(new LazyMiddleware($container, 'errorHandler.middleware'));
-
-if ($container['debug']) {
-    $app->add(new LazyMiddleware($container, ProphilerMiddleware::class));
-}
