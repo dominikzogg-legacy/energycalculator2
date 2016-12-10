071fe49b4e79dc98bf2ef1d5da9af273dd3509c3
Dominik Zogg <dominik.zogg@gmail.com>
2016-12-10 15:08:11 +0100

    do not use cache in dev en

--- app/Provider/TwigProvider.php
+++ app/Provider/TwigProvider.php
@@ -26,9 +26,8 @@ final class TwigProvider implements ServiceProviderInterface
 
         $container['twig'] = function () use ($container) {
             $twig = new \Twig_Environment($container['twig.loader'], [
-                'cache' => $container['cacheDir'].'/twig',
-                'debug' => $container['debug'],
-                'auto_reload' => !$container['debug'] ? null : true,
+                'cache' => !$container['debug'] ? $container['cacheDir'].'/twig': null,
+                'debug' => $container['debug']
             ]);
 
             foreach ($container['twig.extensions'] as $extension) {
